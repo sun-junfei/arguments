14c6a80bbbaa6071cae0a39a6b651712444fe1cd
w0212 <sjf2002@sohu.com>
2023-05-31 12:43:23 -0700

    initial commit

---
+++ src/components/Workspace.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Toolbox from "./Toolbox";
+
+function Workspace(props) {
+  return (
+    <div>
+      <Toolbox />
+    </div>
+  );
+}
+
+export default Workspace;
