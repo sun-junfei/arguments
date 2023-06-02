1b3468c51916723529ec4b218ada598f8ae49f73
w0212 <sjf2002@sohu.com>
2023-06-02 15:41:57 -0700

    now resizable, retractable

--- src/components/Util.js
+++ src/components/Util.js
@@ -45,4 +45,20 @@ const useAutosizeTextArea = (defBoxRef, textAreaRef, value) => {
   }, [textAreaRef, value]);
 };
 
-export { useMousePosition, useAutosizeTextArea };
+const useAutosizeInput = (termboxRef, inputRef, value) => {
+  useEffect(() => {
+    if (inputRef) {
+      // We need to reset the width momentarily to get the correct scrollHeight for the textarea
+      inputRef.style.width = "0px";
+      const scrollWidth = inputRef.scrollWidth;
+      inputRef.style.width = "95%";
+      const termBoxWidth = scrollWidth * 2;
+
+      // We then set the height directly, outside of the render loop
+      // Trying to set this with state or a ref will product an incorrect value.
+      termboxRef.style.width = termBoxWidth + "px";
+    }
+  }, [inputRef, value]);
+};
+
+export { useMousePosition, useAutosizeTextArea, useAutosizeInput };
